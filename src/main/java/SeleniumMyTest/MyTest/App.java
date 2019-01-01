299ce7398dfd9919af89468520cd9ba2c8e75df3
Mittal <amitta4@ms.ds.uhc.com>
2019-01-01 05:35:09 +0530

    modified jar

--- src/main/java/SeleniumMyTest/MyTest/App.java
+++ src/main/java/SeleniumMyTest/MyTest/App.java
@@ -20,7 +20,7 @@ public class App
     {
     	WebDriver driver;
 //		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","/media/sf_shared/chromedriver");
+		System.setProperty("webdriver.chrome.driver","chromedriver");
 		
 		ChromeOptions chromeoptions = new ChromeOptions();
 		chromeoptions.addArguments("--headless"); // headless mode for jenkins
